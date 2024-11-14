9948f8d25135c412718da02903a0acae5c106172
Hicham02145872 <hichamaltit91@gmail.com>
2024-11-14 07:26:46 -0800

    Update LeaveRequestModel

--- app/Models/LeaveRequestModel.php
+++ app/Models/LeaveRequestModel.php
@@ -3,10 +3,12 @@
 // app/Models/LeaveRequestModel.php
 namespace App\Models;
 
+use App\Entities\LeaveRequest;
 use CodeIgniter\Model;
 
 class LeaveRequestModel extends Model
 {
     protected $table = 'leave_requests';
+    protected $returnType = LeaveRequest::class;
     protected $allowedFields = ['employee_name', 'start_date', 'end_date', 'reason', 'status'];
 }
