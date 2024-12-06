3700ce001c54c649e33edadc926adbe7adc091aa
Hicham02145872 <154740179+Hicham02145872@users.noreply.github.com>
2024-12-06 10:18:41 -0800

    Delete app/Views/register.php

--- app/Views/register.php
+++
@@ -1,6 +0,0 @@
-<form action="/register/store" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="email" name="email" placeholder="Email">
-    <input type="password" name="password" placeholder="Password">
-    <button type="submit">Register</button>
-</form>
